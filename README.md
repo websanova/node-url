3004c3976cbbfd3c355dd1e60d999cc6f820efd4
websanova <rob@websanova.com>
2014-03-27 13:22:10 -0400

    Updated to "wurl" to avoid naming conflict in npm.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# url()
+# wurl()
 
 A simple url parsing library for Node.js.
 
@@ -11,62 +11,48 @@ A simple url parsing library for Node.js.
 For path(1) and path(-1) will always act as if the path is in the form `/some/path/` regardless of whether the original path was `/some/path` or `/some/path/`.
 
 
+## Install
+
+```
+npm install wurl
+```
+
 ## Examples
 
 ```js
+var url = require('url');
 var testURL = 'http://rob:abcd1234@www.example.com/path/index.html?query1=test&silly=willy#test=hash&chucky=cheese';
 
-url('domain', testURL);    // example.com
-url('hostname', testURL);  // www.example.com
-url('sub', testURL);       // www
-url('.0', testURL);        // (an empty string)
-url('.1', testURL);        // www
-url('.2', testURL);        // example
-url('.-1', testURL);       // com
-url('auth', testURL);      // rob:abcd1234
-url('user', testURL);      // rob
-url('pass', testURL);      // abcd1234
-url('port', testURL);      // 80
-url('protocol', testURL);  // http
-url('path', testURL);      // /path/index.html
-url('file', testURL);      // index.html
-url('filename', testURL);  // index
-url('fileext', testURL);   // html
-url('1', testURL);         // path
-url('2', testURL);         // index.html
-url('3', testURL);         // (an empty string)
-url('-1', testURL);        // index.html
-url(1, testURL);           // path
-url(2, testURL);           // index.html
-url(-1, testURL);          // index.html
-url('?', testURL);         // query1=test&silly=willy
-url('?silly', testURL);    // willy
-url('?poo', testURL);      // null
-url('#', testURL);         // test=hash&chucky=cheese
-url('#chucky', testURL);   // cheese
-url('#poo', testURL);      // null
+wurl('domain', testURL);    // example.com
+wurl('hostname', testURL);  // www.example.com
+wurl('sub', testURL);       // www
+wurl('.0', testURL);        // (an empty string)
+wurl('.1', testURL);        // www
+wurl('.2', testURL);        // example
+wurl('.-1', testURL);       // com
+wurl('auth', testURL);      // rob:abcd1234
+wurl('user', testURL);      // rob
+wurl('pass', testURL);      // abcd1234
+wurl('port', testURL);      // 80
+wurl('protocol', testURL);  // http
+wurl('path', testURL);      // /path/index.html
+wurl('file', testURL);      // index.html
+wurl('filename', testURL);  // index
+wurl('fileext', testURL);   // html
+wurl('1', testURL);         // path
+wurl('2', testURL);         // index.html
+wurl('3', testURL);         // (an empty string)
+wurl('-1', testURL);        // index.html
+wurl(1, testURL);           // path
+wurl(2, testURL);           // index.html
+wurl(-1, testURL);          // index.html
+wurl('?', testURL);         // query1=test&silly=willy
+wurl('?silly', testURL);    // willy
+wurl('?poo', testURL);      // null
+wurl('#', testURL);         // test=hash&chucky=cheese
+wurl('#chucky', testURL);   // cheese
+wurl('#poo', testURL);      // null
 ```
-
-We can also pass a url in and use all the same options on it:
-
-```js
-url('domain', 'test.www.example.com/path/here');            // example.com
-url('hostname', 'test.www.example.com/path/here');          // test.www.example.com
-url('sub', 'test.www.example.com/path/here');               // test.www
-url('protocol', 'www.example.com/path/here');               // http
-url('path', 'http://www.example.com:8080/some/path');       // /some/path
-url('port', 'http://www.example.com:8080/some/path');       // 8080
-url('protocol', 'https://www.example.com:8080/some/path');  // https
-etc...
-```
-
-
-## Resources
-
-* [More jQuery plugins by Websanova](http://websanova.com/plugins)
-* [Websanova JavaScript Extensions Project](http://websanova.com/extensions)
-* [jQuery Plugin Development Boilerplate](http://wboiler.websanova.com)
-* [The Ultimate Guide to Writing jQuery Plugins](http://www.websanova.com/blog/jquery/the-ultimate-guide-to-writing-jquery-plugins)
 
 
 ## License
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
  * Usage: node test.js
  */
 
-var url = require('./url.js'),
+var wurl = require('./wurl.js'),
     assert = require('assert');
 
 function strictEqual(a, b) {
@@ -16,108 +16,108 @@ var testUrl = 'http://rob:abcd1234@www.domain.com/path/index.html?query1=test&si
     testIp = 'https://rob:abcd1234@1.2.3.4/path/index.html?query1=test&silly=willy#test=hash&chucky=cheese';
 
 // Test dead values.
-strictEqual(url('', testUrl), testUrl);
-strictEqual(url(null, testUrl), testUrl);
-strictEqual(url(undefined, testUrl), testUrl);
-strictEqual(url('domain'), undefined);
-strictEqual(url('domain', ''), undefined);
-strictEqual(url('domain', null), undefined);
-strictEqual(url('domain', undefined), undefined);
+strictEqual(wurl('', testUrl), testUrl);
+strictEqual(wurl(null, testUrl), testUrl);
+strictEqual(wurl(undefined, testUrl), testUrl);
+strictEqual(wurl('domain'), undefined);
+strictEqual(wurl('domain', ''), undefined);
+strictEqual(wurl('domain', null), undefined);
+strictEqual(wurl('domain', undefined), undefined);
 
 // Test domain.
-strictEqual(url('domain', testUrl), 'domain.com');
-strictEqual(url('domain', testHttps), 'domain.com');
-strictEqual(url('domain', testIp), '1.2.3.4');
+strictEqual(wurl('domain', testUrl), 'domain.com');
+strictEqual(wurl('domain', testHttps), 'domain.com');
+strictEqual(wurl('domain', testIp), '1.2.3.4');
 
 // Test hostname.
-strictEqual(url('hostname', testUrl), 'www.domain.com');
-strictEqual(url('hostname', testHttps), 'www.domain.com');
-strictEqual(url('hostname', testIp), '1.2.3.4');
+strictEqual(wurl('hostname', testUrl), 'www.domain.com');
+strictEqual(wurl('hostname', testHttps), 'www.domain.com');
+strictEqual(wurl('hostname', testIp), '1.2.3.4');
 
 // Test sub.
-strictEqual(url('sub', testUrl), 'www');
+strictEqual(wurl('sub', testUrl), 'www');
 
 // Test domain index.
-strictEqual(url('.0', testUrl), '');
-strictEqual(url('.1', testUrl), 'www');
-strictEqual(url('.2', testUrl), 'domain');
-strictEqual(url('.-1', testUrl), 'com');
+strictEqual(wurl('.0', testUrl), '');
+strictEqual(wurl('.1', testUrl), 'www');
+strictEqual(wurl('.2', testUrl), 'domain');
+strictEqual(wurl('.-1', testUrl), 'com');
 
 // Test auth.
-strictEqual(url('auth', testUrl), 'rob:abcd1234');
+strictEqual(wurl('auth', testUrl), 'rob:abcd1234');
 
 // Test user.
-strictEqual(url('user', testUrl), 'rob');
+strictEqual(wurl('user', testUrl), 'rob');
 
 // Test port.
-strictEqual(url('port', testUrl), '80');
-strictEqual(url('port', testUrl.toUpperCase() ), '80');
-strictEqual(url('port', "http://example.com:80" ), '80');
-strictEqual(url('port', testHttps ), '443');
-strictEqual(url('port', testHttps.toUpperCase() ), '443');
-strictEqual(url('port', "https://example.com:443" ), '443');
+strictEqual(wurl('port', testUrl), '80');
+strictEqual(wurl('port', testUrl.toUpperCase() ), '80');
+strictEqual(wurl('port', "http://example.com:80" ), '80');
+strictEqual(wurl('port', testHttps ), '443');
+strictEqual(wurl('port', testHttps.toUpperCase() ), '443');
+strictEqual(wurl('port', "https://example.com:443" ), '443');
 
 // Test pass.
-strictEqual(url('pass', testUrl), 'abcd1234');
+strictEqual(wurl('pass', testUrl), 'abcd1234');
 
 // Test protocol.
-strictEqual(url('protocol', testUrl), 'http');
+strictEqual(wurl('protocol', testUrl), 'http');
 
 // Test path.
-strictEqual(url('path', testUrl), '/path/index.html');
-strictEqual(url('path', 'http://www.domain.com/first/second' ), '/first/second');
-strictEqual(url('path', 'http://www.domain.com/first/second/' ), '/first/second/');
-strictEqual(url('path', 'http://www.domain.com:8080/first/second' ), '/first/second');
-strictEqual(url('path', 'http://www.domain.com:8080/first/second/' ), '/first/second/');
-strictEqual(url('path', 'http://www.domain.com/first/second?test=foo' ), '/first/second');
-strictEqual(url('path', 'http://www.domain.com/first/second/?test=foo' ), '/first/second/');
-strictEqual(url('path', 'http://www.domain.com/path#anchor' ), '/path');
-strictEqual(url('path', 'http://www.domain.com/path/#anchor' ), '/path/');
-strictEqual(url('path', 'http://www.domain.com' ), '');
-strictEqual(url('path', 'http://www.domain.com/' ), '/');
-strictEqual(url('path', 'http://www.domain.com#anchor' ), '');
-strictEqual(url('path', 'http://www.domain.com/#anchor' ), '/');
-strictEqual(url('path', 'http://www.domain.com?test=foo' ), '');
-strictEqual(url('path', 'http://www.domain.com/?test=foo' ), '/');
-strictEqual(url('path', 'http://www.domain.com:80' ), '');
-strictEqual(url('path', 'http://www.domain.com:80/' ), '/');
-strictEqual(url('path', 'http://www.domain.com:80#anchor' ), '');
-strictEqual(url('path', 'http://www.domain.com:80/#anchor' ), '/');
-strictEqual(url('path', 'http://www.domain.com:80?test=foo' ), '');
-strictEqual(url('path', 'http://www.domain.com:80/?test=foo' ), '/');
+strictEqual(wurl('path', testUrl), '/path/index.html');
+strictEqual(wurl('path', 'http://www.domain.com/first/second' ), '/first/second');
+strictEqual(wurl('path', 'http://www.domain.com/first/second/' ), '/first/second/');
+strictEqual(wurl('path', 'http://www.domain.com:8080/first/second' ), '/first/second');
+strictEqual(wurl('path', 'http://www.domain.com:8080/first/second/' ), '/first/second/');
+strictEqual(wurl('path', 'http://www.domain.com/first/second?test=foo' ), '/first/second');
+strictEqual(wurl('path', 'http://www.domain.com/first/second/?test=foo' ), '/first/second/');
+strictEqual(wurl('path', 'http://www.domain.com/path#anchor' ), '/path');
+strictEqual(wurl('path', 'http://www.domain.com/path/#anchor' ), '/path/');
+strictEqual(wurl('path', 'http://www.domain.com' ), '');
+strictEqual(wurl('path', 'http://www.domain.com/' ), '/');
+strictEqual(wurl('path', 'http://www.domain.com#anchor' ), '');
+strictEqual(wurl('path', 'http://www.domain.com/#anchor' ), '/');
+strictEqual(wurl('path', 'http://www.domain.com?test=foo' ), '');
+strictEqual(wurl('path', 'http://www.domain.com/?test=foo' ), '/');
+strictEqual(wurl('path', 'http://www.domain.com:80' ), '');
+strictEqual(wurl('path', 'http://www.domain.com:80/' ), '/');
+strictEqual(wurl('path', 'http://www.domain.com:80#anchor' ), '');
+strictEqual(wurl('path', 'http://www.domain.com:80/#anchor' ), '/');
+strictEqual(wurl('path', 'http://www.domain.com:80?test=foo' ), '');
+strictEqual(wurl('path', 'http://www.domain.com:80/?test=foo' ), '/');
 
 // Test file.
-strictEqual(url('file', testUrl), 'index.html');
-strictEqual(url('filename', testUrl), 'index');
-strictEqual(url('fileext', testUrl), 'html');
+strictEqual(wurl('file', testUrl), 'index.html');
+strictEqual(wurl('filename', testUrl), 'index');
+strictEqual(wurl('fileext', testUrl), 'html');
 
 // Test path index.
-strictEqual(url('1', testUrl), 'path');
-strictEqual(url(1, testUrl), 'path');
-strictEqual(url('2', testUrl), 'index.html');
-strictEqual(url('3', testUrl), '');
-strictEqual(url('-1', testUrl), 'index.html');
-strictEqual(url('1', 'http://www.domain.com/first/second' ), 'first');
-strictEqual(url('1', 'http://www.domain.com/first/second/' ), 'first');
-strictEqual(url('-1', 'http://www.domain.com/first/second?test=foo' ), 'second');
-strictEqual(url('-1', 'http://www.domain.com/first/second/?test=foo' ), 'second');
+strictEqual(wurl('1', testUrl), 'path');
+strictEqual(wurl(1, testUrl), 'path');
+strictEqual(wurl('2', testUrl), 'index.html');
+strictEqual(wurl('3', testUrl), '');
+strictEqual(wurl('-1', testUrl), 'index.html');
+strictEqual(wurl('1', 'http://www.domain.com/first/second' ), 'first');
+strictEqual(wurl('1', 'http://www.domain.com/first/second/' ), 'first');
+strictEqual(wurl('-1', 'http://www.domain.com/first/second?test=foo' ), 'second');
+strictEqual(wurl('-1', 'http://www.domain.com/first/second/?test=foo' ), 'second');
 
 // Test query string param
-strictEqual(url('?', testUrl), 'query1=test&silly=willy');
-strictEqual(url('?silly', testUrl), 'willy');
-strictEqual(url('?poo', testUrl), null);
-strictEqual(url('?poo', 'http://domain.com?poo=' ), '');
-strictEqual(url('?poo', 'http://domain.com/?poo' ), '');
-strictEqual(url('?poo', 'http://domain.com?poo' ), '');
-strictEqual(url('?poo', 'http://domain.com?' ), null);
-strictEqual(url('?poo', 'http://domain.com' ), null);
+strictEqual(wurl('?', testUrl), 'query1=test&silly=willy');
+strictEqual(wurl('?silly', testUrl), 'willy');
+strictEqual(wurl('?poo', testUrl), null);
+strictEqual(wurl('?poo', 'http://domain.com?poo=' ), '');
+strictEqual(wurl('?poo', 'http://domain.com/?poo' ), '');
+strictEqual(wurl('?poo', 'http://domain.com?poo' ), '');
+strictEqual(wurl('?poo', 'http://domain.com?' ), null);
+strictEqual(wurl('?poo', 'http://domain.com' ), null);
 
 // Test hash string param.
-strictEqual(url('#', testUrl), 'test=hash&chucky=cheese');
-strictEqual(url('#chucky', testUrl), 'cheese');
-strictEqual(url('#poo', testUrl), null);
-strictEqual(url('#poo', 'http://domain.com#poo=' ), '');
-strictEqual(url('#poo', 'http://domain.com/#poo' ), '');
-strictEqual(url('#poo', 'http://domain.com#poo' ), '');
-strictEqual(url('#poo', 'http://domain.com#' ), null);
-strictEqual(url('#poo', 'http://domain.com' ), null);
+strictEqual(wurl('#', testUrl), 'test=hash&chucky=cheese');
+strictEqual(wurl('#chucky', testUrl), 'cheese');
+strictEqual(wurl('#poo', testUrl), null);
+strictEqual(wurl('#poo', 'http://domain.com#poo=' ), '');
+strictEqual(wurl('#poo', 'http://domain.com/#poo' ), '');
+strictEqual(wurl('#poo', 'http://domain.com#poo' ), '');
+strictEqual(wurl('#poo', 'http://domain.com#' ), null);
+strictEqual(wurl('#poo', 'http://domain.com' ), null);
